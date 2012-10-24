085c6a8fc8bd60f481817257c9051d49a1792f41
Doug Turnbull <dturnbull@o19s.com>
2012-10-24 14:03:21 -0400

    Example with jsonify to play with backbone

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import flask
 
 app = flask.Flask("Hello World")
+count = 0
 
 
 @app.route("/")
 def hello():
     resp = flask.render_template("index.html", name="Doug")
     return resp
+
+
+@app.route("/counter")
+def counter():
+    global count
+    print count
+    count += 1
+    return flask.jsonify({"Count": count})
 
 if __name__ == "__main__":
     app.run()
